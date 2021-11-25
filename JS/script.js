99871388fae7a289e39db642778e765485d88edc
jani-e <j.eriksson91@gmail.com>
2021-11-25 19:11:24 +0200

    function getTopArtists added

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,19 @@
+const api = getApiKey(); //apikey temporary solution
+let targetdiv = document.getElementById('testdiv');
+
 function test() {
-    let targetdiv = document.getElementById('testdiv');
     let url = "https://www.finnkino.fi/xml/TheatreAreas/";
     let xmlhttp = new XMLHttpRequest();
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            //let data = JSON.parse(xmlhttp.responseText);
             let data = xmlhttp.responseXML;
             console.log(data);
         }
     }
 }
 function testLastFM() {
-    let targetdiv = document.getElementById('testdiv');
-    const api = "x";
     let url = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Cher&api_key=" + api + "&format=json";
     let xmlhttp = new XMLHttpRequest();
     xmlhttp.open("GET", url, true);
@@ -23,6 +22,25 @@ function testLastFM() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             let data = JSON.parse(xmlhttp.responseText);
             console.log(data);
+        }
+    }
+}
+let data = null;
+function getTopArtists() {
+    const url = "http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=" + api + "&format=json";
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            data = JSON.parse(xmlhttp.responseText);
+            let list = "<ul>";
+            console.log(data)
+            for (i = 0; i < data.artists.artist.length; i++) {
+                list += "<li>" + data.artists.artist[i].name + "</li>";
+            }
+            list += "</ul>";
+            targetdiv.innerHTML = list;
         }
     }
 }
