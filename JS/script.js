f59e7ed72dc1b122fdf4c01a6a689b5e087ddf61
jani-e <j.eriksson91@gmail.com>
2021-12-09 18:04:21 +0200

    api key netlify

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-const api = getApiKey(); //apikey temporary solution
+const api = API_KEY;
 let currentArtistName = null; //keeping record of current artist
 let resultLimit = 20; //limiting results from server response
 getArtists(); //functions starts once website is loaded fully
